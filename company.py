a898854d63cf52ce794b175cb20303b83ddc7103
A <akelloscovia907@gmail.com>
2025-02-12 13:27:18 +0300

    author and book classes

---
+++ company.py
@@ -0,0 +1,40 @@
+#company class
+class Company:
+    #Contructor
+    def _init_(self,name,email,contact):
+        self.name = name
+        self.email = email
+        self.contact = contact
+        
+        #new instance
+        company_one = Company('scovia','akelloscovia907@gmail.com',7807407490)
+        print(company_one)
+        
+        #ass
+        #complete author and book classes
+        #atleast 5 properties  and functions.
+        
+class Author:
+    def __init__(self, name, birth_year, Age, contact, nationality):
+        self.name = name
+        self.birth_year = birth_year
+        self.Age = Age
+        self.contact = contact
+        self.nationality = nationality
+        self.books = []
+
+    def __str__(self):
+        return f"{self.name} ({self.birth_year}, {self.Age}, {self.contact} ,{self.nationality})"
+
+
+class Book:
+    def __init__(self, title, publication_year, author, language, edition):
+        self.title = title
+        self.publication_year = publication_year
+        self.author = author
+        self.language = language
+        self.edition = edition
+        author.add_book(self)
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author.name} ({self.publication_year} {self.language},{self.edition})"
